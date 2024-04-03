f9125c10c34f2c82b9a8f1c840d6723a7701dbe6
Md Jehid Alam <146694343+mdjehidalam@users.noreply.github.com>
2024-04-03 09:35:41 +0530

    Create Print size of int, char, float, double.cpp

---
+++ Print size of int, char, float, double.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+     // write a progream to print size of int, char , double, float;
+     int i;
+     char c;
+     double d;
+     float f;
+     cout<<"size of int "<<sizeof(i)<<" bytes"<<endl;
+     cout<<"size of char: "<<sizeof(c)<<"bytes"<<endl;
+     cout<<"size of double: "<<sizeof(d)<<"bytes"<<endl;
+     cout<<"size of float :"<<sizeof(f)<<"bytes"<<endl;
+}
