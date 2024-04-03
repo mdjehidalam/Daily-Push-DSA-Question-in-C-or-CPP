69fdbc4c889f8b64d86bb67490884b9a99ddaff1
Md Jehid Alam <146694343+mdjehidalam@users.noreply.github.com>
2024-04-03 09:29:18 +0530

    Create Swap two Numbers.cpp

---
+++ Swap two Numbers.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+     //swap two number 
+     int a,b;
+     cin>>a>>b;
+     int storeOfa=a;
+     a=b;
+     b=storeOfa;
+     cout<<"After wapping two Numbers : "<<endl;
+     cout<<"a:"<<a<<endl<<"b:"<<b<<endl;
+}
