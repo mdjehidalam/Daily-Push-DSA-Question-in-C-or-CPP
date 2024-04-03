47f50550381f15fdfa37ccb05284655c8caf1fc0
Md Jehid Alam <146694343+mdjehidalam@users.noreply.github.com>
2024-04-03 09:25:19 +0530

    Create Print ASSCI Value.cpp

---
+++ Print ASSCI Value.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    char c;
+    cout<<"ASSCI Value: "<<endl;
+    cin>>c;
+    cout<<(int)c<<endl;//explicit conversation 
+    //second way to conversation;
+    int n=c;
+    cout<<n<<endl;
+}
